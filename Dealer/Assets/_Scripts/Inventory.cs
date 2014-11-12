5e17ae732a873c6c6aab34c1be0640f065c8aee7
Tubocass <tubocass@gmail.com>
2014-11-12 01:35:56 -0600

    Initial project commit. imported sample assets. Added Item scripts

---
+++ Dealer/Assets/_Scripts/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+
+	public List<Item> inventory = new List<Item> ();
+	private Item_Database itemDB;
+
+	 
+	void Awake()
+	{
+		itemDB = GameObject.FindGameObjectWithTag ("ItemDatabase").GetComponent <Item_Database> ();
+		print(itemDB.items[0].itemName);
+		inventory.Add (itemDB.items[0]);
+		inventory.Add (itemDB.items[1]);
+
+	}
+	void Start()
+	{}
+
+	void OnGUI()
+	{
+		for(int i = 0;i<inventory.Count;i++)
+		{
+			GUI.Label(new Rect(10,i*30,800,200),inventory[i].itemName);
+		}
+
+	}
+}
---
+++ Dealer/Assets/_Scripts/Item_Database.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Item_Database : MonoBehaviour 
+{
+	public List<Item> items = new List<Item> ();
+
+}
