1517b766b8bf43adec24d282ec372dab9e41c6a3
BoB <tubocass@gmail.com>
2014-11-18 22:00:02 -0600

    Added menu to open up trade with NPC. *Trade not possible between inventories just yet.

--- Dealer/Assets/_Scripts/NPC.cs
+++ Dealer/Assets/_Scripts/NPC.cs
@@ -4,17 +4,41 @@ using System.Collections;
 public class NPC : MonoBehaviour {
 
 	Inventory inventory;
+	bool clicked;
+	SpriteRenderer sprite;
 	// Use this for initialization
 	void Start () 
 	{
+		sprite = GetComponent<SpriteRenderer>();
 		inventory = GetComponent<Inventory>();
 		inventory.AddItem(1);
 	}
-	
+	void OnGUI()
+	{
+		Rect trade = new Rect(40,200,100,40);
+		Event e = Event.current;
+		if(!sprite.bounds.Contains(e.mousePosition)&& !trade.Contains(e.mousePosition)&& e.type==EventType.mouseDown&& e.button==0)
+		{
+			//print ("tiiiittttss");
+			clicked = false;
+		}
+		if(clicked)
+		{
+			if(GUI.Button(trade,"trade"))
+			{
+				inventory.showInventory = !inventory.showInventory; 
+			}
+		}
+	}
 	void OnMouseDown() 
 	{
 		print ("bboobbss");
-		inventory.showInventory = true;
-		inventory.AddItem(1);
+		clicked = true;
+		//inventory.showInventory = true;
+		//inventory.AddItem(1);
+	}
+	void Update()
+	{
+
 	}
 }
