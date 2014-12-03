7a05999245f5019bd857ae6ae3c55d1a388e2c9b
J Doh <jdohgamer@gmail.com>
2014-12-03 00:15:13 -0600

    Update code.Add Door.Add Scene Change ("n")

--- Dealer/Assets/_Scripts/Player_Interactions.cs
+++ Dealer/Assets/_Scripts/Player_Interactions.cs
@@ -7,37 +7,49 @@ public class Player_Interactions : MonoBehaviour {
 	
 	void Start()
 	{
+
+		DontDestroyOnLoad (this.gameObject);
 		inventory = GetComponent<Player_Inventory>();
 		inventory.AddItem(1);
 		inventory.AddItem(2);
 	}
 	void OnTriggerEnter2D(Collider2D other)
 	{
-		switch(other.gameObject.tag)
-		{
+		switch (other.gameObject.tag) {
 		
-		case "Weed":
-		{
-			Destroy(other.gameObject,1);
-			print ("something");
-			inventory.AddItem(1);
-			break;
-		}
-		case "Drank":
-		{
-			Destroy(other.gameObject,1);
-			print ("something");
-			inventory.AddItem(2);
-			break;
-		}
-		case "Pills":
-		{
-			Destroy(other.gameObject,1);
-			print ("something");
-			inventory.AddItem(2);
-			break;
-		}
-		}
-		
+				case "Weed":
+						{
+								Destroy (other.gameObject, 1);
+								print ("something");
+								inventory.AddItem (1);
+								break;
+						}
+				case "Drank":
+						{
+								Destroy (other.gameObject, 1);
+								print ("something");
+								inventory.AddItem (2);
+								break;
+						}
+				case "Pills":
+						{
+								Destroy (other.gameObject, 1);
+								print ("something");
+								inventory.AddItem (2);
+								break;
+						}
+				case "Door":
+						{
+								Destroy (other.gameObject);
+								break;
+						}
+				}
+	
+
 	}
+
+
+	
+
 }
+
---
+++ Dealer/Assets/_Scripts/SceneChange.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneChange : MonoBehaviour {
+
+
+	void Update () {
+
+			if (Input.GetKeyDown ("n"))
+			{
+				Application.LoadLevel ("Scene_1");
+				transform.position = new Vector3(0,0,0);
+				
+			}
+		}
+	}
+
