724f3a44f3e7df09cc43c97cb1b454c4ed2dd4eb
J Doh <jdohgamer@gmail.com>
2015-01-29 21:30:45 -0600

    Added 'K' swing aim. Uses Raycasts to check for objects in front of it.

--- Dealer/Assets/_Scripts/Player_Interactions.cs
+++ Dealer/Assets/_Scripts/Player_Interactions.cs
@@ -12,9 +12,13 @@ public class Player_Interactions : MonoBehaviour
 	Animator anim;
 	int idleHash = Animator.StringToHash("Base Layer.Idle");
 	int tokeHash = Animator.StringToHash("Base Layer.Toking");
-
-
-
+	int Damage = 50;
+	float Distance;
+	float MaxDistance = 2;
+	Transform TheSystem;
+	LayerMask characterMask;
+	
+	
 	//public static event TradeAction SoldWeed;
 	
 	void Start()
@@ -22,6 +26,7 @@ public class Player_Interactions : MonoBehaviour
 		inventory = GetComponent<Old_Inventory>();
 		anim = GetComponent<Animator>();
 		journal = GetComponent<Quest_Journal>();
+		characterMask = 1<<9;
 
 		inventory.AddItem(1);
 		inventory.AddItem(2);
@@ -30,14 +35,24 @@ public class Player_Interactions : MonoBehaviour
 		//journal.AddItem(2);
 	}
 
-	void FixedUpdate()
-	{
-		if(Input.GetKey (KeyCode.K)) {
+	void Update() {
+		//This is for the swinging animation and for the player to check to see if an NPC is in front of him
+
+		if(Input.GetKeyDown (KeyCode.K)) {
 			anim.SetBool("SwingAnim", true);
-		}
-		else {
-			anim.SetBool("SwingAnim", false);
-		}
+
+			RaycastHit2D hit = Physics2D.Raycast (transform.position, transform.up,5, characterMask ); 
+			//print (hit.collider.gameObject.tag);
+			// add an action for a specific tag here
+			if(hit.collider.gameObject.tag == "NPC"){
+
+				Debug.Log ("boom, bitch"); 
+			}
+
+			}
+			else {
+				anim.SetBool("SwingAnim", false);
+					}
 
 
 	}
