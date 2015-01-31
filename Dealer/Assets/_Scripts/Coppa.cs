638e7faa73b44d0578c52646a73afbf921403405
BoB <tubocass@gmail.com>
2015-01-31 16:21:35 -0600

    Made Coppa LoS a child object that sits on a different layer from "Characters". This way the giant collider doesn't cover up other NPC colliders in a raycast.

--- Dealer/Assets/_Scripts/Coppa.cs
+++ Dealer/Assets/_Scripts/Coppa.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Coppa : MonoBehaviour 
@@ -9,53 +9,30 @@ public class Coppa : MonoBehaviour
 	Transform tran;
 	public LayerMask visionMask;
 	GameObject player;
+	public Vector3 lastKnownLoc;
 
 	// Use this for initialization
 	void Start () 
 	{
-		col = GetComponent<CircleCollider2D>();
+		col = GetComponentInChildren<CircleCollider2D>();
 		tran = transform;
 		player = GameObject.FindGameObjectWithTag("Player");
 	}
-
-
-	IEnumerator Move(Vector3 dir)
+	
+	public IEnumerator Move(Vector3 dir)
 	{
 		tran.position = Vector3.MoveTowards(tran.position,dir,.2f);
 		Debug.Log("moving");
-		yield return null;
+		yield return new WaitForSeconds(2);
+	}
+	void FixedUpdate()
+	{
+		if(bPlayerVisible)
+		{
+			StartCoroutine("Move",lastKnownLoc);
+		}else StopCoroutine("Move");
 	}
 
-	void OnTriggerStay2D(Collider2D other)
-	{
-		if(other.gameObject.tag == "Player")
-		{
-			Vector3 direction = other.transform.position - tran.position;
-			float angle = Vector3.Angle(direction, tran.up);
-			//Is the object in my field of view?
-			if(angle < fieldOfViewAngle*0.5f)
-			{
-				//are there any obstructions between me and the object?
-				//Vector3 fwd = tran.TransformDirection(Vector3.up*1);
-				RaycastHit2D hit = Physics2D.Raycast(tran.position, direction,length,visionMask);
-				if(hit.collider!=null && hit.collider.gameObject.tag == "Player")
-				{
-					Debug.DrawRay(tran.position, direction, Color.red);
-					//Debug.Log("you're a player");
-					bPlayerVisible = true;
-					StartCoroutine("Move",(player.transform.position));
-				}else{ bPlayerVisible = false; StopCoroutine("Move");}
-			}
-		}
-	}
-		
-	void OnTriggerExit2D(Collider2D other)
-	{
-		if(other.gameObject.tag == "Player")
-		{
-			bPlayerVisible = false;
-		}
-	}
 }
 			
 
