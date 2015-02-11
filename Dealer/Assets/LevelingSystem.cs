3728bfad85a984cd33cacca5af8df45c948ef8bf
J Doh <jdohgamer@gmail.com>
2015-02-10 19:10:34 -0600

    Selling Weed Increases Level.

--- Dealer/Assets/LevelingSystem.cs
+++ Dealer/Assets/LevelingSystem.cs
@@ -1,65 +1,45 @@
-﻿using UnityEngine;
+ using UnityEngine;
 using System.Collections;
 
 public class LevelingSystem : MonoBehaviour {
 
-
+	public string xpText;
 	int curXp = 0;
 	int maxXp = 100;
-
-	public string xpText;
 	int level = 1;
+	int totalWeed = 0;
 
-	// Use this for initialization
+
 	void Start () {
-	
-	}
+		GameObject player = GameObject.FindWithTag ("Player");
+		player.GetComponent<Old_Inventory>().SoldWeed+=LevelWeed;
+		}
 
 	void OnGUI()
 	{
-		GUI.Box(new Rect(100,100,140,140),""+(xpText));
+		GUI.Box(new Rect(100,100,180,180),""+(xpText));
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-	
 		xpText = "Level: " + level + "    XP: " + curXp + " / " + maxXp;
 
-	
-
-
-	if(curXp == maxXp) {
-	
+		if(curXp == maxXp)
+		{
 			levelUpSystem();
 		}
+			}
 
-	
-
-		if(Input.GetKeyDown (KeyCode.Z)) {
-			Debug.Log ("LEVEL UP!");
-			curXp += 10;
-	
-		}
+	void LevelWeed() 
+		{
+			print ("DING!");
+			level++;
 		}
 
-
-
-
-
-	void levelUpSystem () {
-
-	
+	void levelUpSystem () 
+	{
 			curXp = 0;
 			level++;
 			maxXp = maxXp + 10;
-
-
-			
-
-
-
-
+	}
 
 		}
-
-}
--- Dealer/Assets/_Scripts/Inventories/Old_Inventory.cs
+++ Dealer/Assets/_Scripts/Inventories/Old_Inventory.cs
@@ -209,6 +209,7 @@ public class Old_Inventory : MonoBehaviour
 			{
 				if(SoldWeed!=null)
 					SoldWeed();
+
 			}
 		}else return;
 	}
